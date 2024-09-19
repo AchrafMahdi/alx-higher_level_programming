f4dcf115eecda0243417790e568b1c2dc93bbadd
ach <ach@example.com>
2024-09-18 21:33:36 -0700

    #10 FIXED

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,4 +7,4 @@ def best_score(a_dictionary):
         if max_score is None or value > max_score:
             max_score = value
             max_key = key
-    return max_key
+    return max_key if max_score is not None else None
