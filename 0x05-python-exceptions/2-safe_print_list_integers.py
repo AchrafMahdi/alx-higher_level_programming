63f3218d18d823abf33b9085f4bf09c796a98ab2
ach <ach@example.com>
2024-09-30 21:31:15 -0700

    #2 FIXED

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except (IndexError):
-            raise IndexError("list index out of range")
-        except (TypeError):
+        except (TypeError, ValueError):
             pass
     print()
     return counter
