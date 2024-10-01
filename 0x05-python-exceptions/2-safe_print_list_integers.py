56ae7a24a649eaa2019198b75a3135163fd0f409
ach <ach@example.com>
2024-09-30 18:32:09 -0700

    Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except (TypeError):
-            pass
         except (IndexError):
             raise IndexError("list index out of range")
+        except (TypeError):
+            pass
     print()
     return counter
